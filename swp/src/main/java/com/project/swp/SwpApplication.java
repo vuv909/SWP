f43c3c93073269d25634a609e36f65fcd5f73f6e
vu minh tu <vuv909@gmail.com>
2023-12-07 20:35:30 +0700

    first commit

---
+++ swp/src/main/java/com/project/swp/SwpApplication.java
@@ -0,0 +1,13 @@
+package com.project.swp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwpApplication.class, args);
+	}
+
+}
---
+++ swp/src/test/java/com/project/swp/SwpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.swp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
